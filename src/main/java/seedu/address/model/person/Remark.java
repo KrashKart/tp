4a223a8e2597036696661509783d6846fb71dd21
KrashKart <e1113755@u.nus.edu>
2024-10-02 01:49:52 +0800

    Edit code for checkStyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -10,20 +10,27 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates Remark instance.
+     * @param remark String remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
     }
+
     @Override
     public String toString() {
         return value;
     }
+
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Remark // instanceof handles nulls
                 && value.equals(((Remark) other).value)); // state check
     }
+
     @Override
     public int hashCode() {
         return value.hashCode();
